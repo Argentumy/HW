4cd2c80e3a08d8ab0c521c893cc53df4c4378a2a
Argentumy <45991715+Argentumy@users.noreply.github.com>
2019-02-23 16:40:54 +0300

    Update 2.3.py

--- 2.3.py
+++ 2.3.py
@@ -1,37 +1,53 @@
-import json
+# import json
+# from pprint import pprint
+# import collections
+#
+# with open('newsafr.json', encoding='utf-8') as file:
+#     news = json.load(file)
+#
+# news2 = news.get("rss", {}).get("channel", {}).get("items", False)
+# string = []
+# string2 = []
+# string3 = []
+#
+# for a in news2:
+#     string.append(a['description'].split(sep=' '))
+#
+# for b in string:
+#     for c in b:
+#         if len(c) >= 6:
+#             string2.append(c)
+#
+# string2.sort(key=len, reverse=True)
+#
+# string3 = collections.Counter(string2).most_common(10)
+# pprint(string3)
+
+
+# Task 2
+
+import xml.etree.ElementTree as ET
+import collections
 from pprint import pprint
 
 
-with open('newsafr.json') as file:
-    news = json.load(file)
-    # pprint(news)
-    # pprint(news['description'])
-    # print(type(news))
-    # pprint(news.values())
+tree = ET.parse('newsafr.xml')
+root = tree.getroot()
+xml_title = root.findall('channel/item/description')
 
-# Посчитать слова в новостях
-# Отсортировать по частоте встречаемости
-# Убрать все слова менее 6 символов
-
-news2 = news.get("rss", {}).get("channel", {}).get("items", False)
 string = []
 string2 = []
-string3 = []
 
-
-
-for a in news2:
-    string.append(a['description'].split(sep=' '))
-# pprint(string)
+for a in xml_title:
+    string.append(a.text.split(sep=' '))
 
 for b in string:
-    # print(b)
     for c in b:
         if len(c) >= 6:
             string2.append(c)
-            # print(len(c))
 
-string3 = string2.sort(key=len, reverse=True)
-pprint(string2)
+string2.sort(key=len, reverse=True)
 
-# pprint(len(string))
+
+string3 = collections.Counter(string2).most_common(10)
+pprint(string3)
